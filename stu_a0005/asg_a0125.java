d47165640238e853997df3cfb4107555faf475b6
Dattaraj <dattaraj311@gmail.com>
2016-06-19 11:46:46 +0530

    find the ceiling

---
+++ stu_a0005/asg_a0125.java
@@ -0,0 +1,17 @@
+class Asg_a0125{
+public static double my_ceiling(double num)
+{
+double x=num;
+int y=(int)x;
+int z=y+1;
+return(z);
+}
+}
+public class asg_a0125{
+public static void main(String args[])
+{
+Asg_a0125 temp=new Asg_a0125();
+System.out.println(Asg_a0125.my_ceiling(10.34));
+}
+}
+
