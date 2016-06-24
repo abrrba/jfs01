59bb01bf19141180e457302561905149ba792de5
abhigiter <abhigiter@gmail.com>
2016-06-24 12:10:03 +0530

    added new folder

---
+++ stu_a0004/asg_a0122/AsgA0122.java
@@ -0,0 +1,16 @@
+class AsgA0122
+{
+public double  is_profit(double selling_price,double cost_price)
+{
+double x=((selling_price-cost_price)/cost_price);
+if(x>0)
+{
+return(x);
+}
+else
+{
+return(0);
+}
+}
+}
+
