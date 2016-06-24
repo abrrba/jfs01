c5a5a1dd028cf00384a0afca51258772943926bc
abhigiter <abhigiter@gmail.com>
2016-06-24 12:01:35 +0530

    added new folder

---
+++ stu_a0004/asg_a0019/AsgA0019.java
@@ -0,0 +1,17 @@
+class Asga0019
+{
+public double circumference(double radius)
+{
+    if(radius>0)
+      {
+        double pi=3.14;
+        double rad=(2*pi*radius);
+        return(rad);
+      }
+	else
+	{
+	return(0);
+	} 
+}
+}
+
