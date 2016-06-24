83473015f28074ccd6044afc2a9215f809a9bb78
dj <djyes9@gmail.com>
2016-06-24 23:17:25 +0530

    Pattern

---
+++ stu_a0003/asg_a0283/AsgA0283.java
@@ -0,0 +1,21 @@
+class AsgA0283{
+public static void main(String argv[])
+{
+int i,j,k;
+for(i=0;i<5;i++){
+        for(j=i;j>=0;j--){
+        System.out.print(" ");}
+        for(k=(2*i);k<9;k++){
+        System.out.print("*");}
+System.out.println("");
+}
+for(i=2;i<6;i++)
+{
+	for(j=i;j<6;j++){
+	System.out.print(" ");}
+	for(k=0;k<(i*2)-1;k++){
+	System.out.print("*");}
+System.out.println("");
+}
+}
+}
