d08911175cffe57e7cfec5864b7534cde1e1f5e1
abhigiter <abhigiter@gmail.com>
2016-06-27 00:46:11 +0530

    vowel

---
+++ stu_a0004/asg_a2205/AsgA2205.java
@@ -0,0 +1,11 @@
+class AsgA2205
+{
+public static void main(String args[])
+{
+String str=new String("O");
+if(str.equalsIgnoreCase("a")||str.equalsIgnoreCase("e")||str.equalsIgnoreCase("i")||str.equalsIgnoreCase("o")||str.equalsIgnoreCase("u"))
+{
+System.out.println("1");
+}
+}
+}
