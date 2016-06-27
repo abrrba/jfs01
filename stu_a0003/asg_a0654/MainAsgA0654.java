28c278aad5f01ae781d32b58d29f2538dd596e16
dj <djyes9@gmail.com>
2016-06-27 18:16:57 +0530

    Find Median

---
+++ stu_a0003/asg_a0654/MainAsgA0654.java
@@ -0,0 +1,8 @@
+public class MainAsgA0654{
+	public static void main(String[] argv)
+	{
+		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		AsgA0654 temp = new AsgA0654();
+		System.out.println(temp.find_median(arr));
+	}
+}
