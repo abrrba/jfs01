58392ec71f30c7a3be1ea46d332f516b1e8883bb
Dattaraj <dattaraj311@gmail.com>
2016-06-28 10:36:31 +0530

    Delete Mainasg_a0031.java

--- stu_a0005/asg_a0031/Mainasg_a0031.java
+++
@@ -1,9 +0,0 @@
-
-public class Mainasg_a0031{
-public static void main(String args[])
-{
-Asg_a0031 obj=new Asg_a0031();
-System.out.println(Asg_a0031.calculate_gross_salary(-20000,10,5000));
-}
-}
-
