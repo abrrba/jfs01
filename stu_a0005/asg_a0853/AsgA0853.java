0099dbdcdaec19341983a2ef587066dd3bc8f47d
Dattaraj <dattaraj311@gmail.com>
2016-06-29 17:27:18 +0530

    check leap year

---
+++ stu_a0005/asg_a0853/AsgA0853.java
@@ -0,0 +1,14 @@
+class AsgA0853{
+	public static int is_leap_year(int year)
+	{
+		if(year%4 == 0)
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+		return(0);
+	}
+}
---
+++ stu_a0005/asg_a0853/MainAsgA0853.java
@@ -0,0 +1,8 @@
+class MainAsgA0853{
+	public static void main(String args[])
+	{
+		AsgA0853 temp = new AsgA0853();
+		temp.is_leap_year(2015);
+	}
+}
+
