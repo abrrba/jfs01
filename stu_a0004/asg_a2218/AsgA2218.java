04691d2e09b11365c33cc8bb48f07436eb36f0ac
abhigiter <abhigiter@gmail.com>
2016-06-29 19:25:54 +0530

    String and array

---
+++ stu_a0004/asg_a2218/AsgA2218.java
@@ -0,0 +1,24 @@
+class AsgA2218
+{
+public static String check_phrase_palindrome(String str)
+{
+String reverse=new String();
+String str1=str.replaceAll("\\s","");
+int count=str1.length();
+for(int i=count-1;i>=0;i--)
+{
+ reverse =reverse + str1.charAt(i);
+}
+if(reverse.equals(str1))
+{
+System.out.print("1");
+return(str);
+}
+else
+{
+System.out.print("0");
+return(str);
+}
+}
+//return(str);
+}
---
+++ stu_a0004/asg_a2219/MainAsgA2219.java
@@ -0,0 +1,9 @@
+public class MainAsgA2219
+{
+public static void main(String args[])
+{
+AsgA2219 temp=new AsgA2219();
+String st=new String("the quick brown fox jumps over the 14 lazy dog");
+temp.check_pangram(st);
+}
+}
