67d0998f75a1590cfd2fcf4c9befe41c395a401b
dj <djyes9@gmail.com>
2016-06-30 12:19:56 +0530

    Array Product

---
+++ stu_a0003/asg_a0651/MainAsgA0651.java
@@ -0,0 +1,7 @@
+public class MainAsgA0651{
+	public static void main(String[] argv){
+		int[] a = {10,20,2,4};
+		AsgA0651 temp = new AsgA0651();
+		System.out.println(temp.product_of_array(a));
+	}
+}
