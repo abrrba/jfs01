df26987a96e8d216a6a65d3a4a3e57034c9fffb0
abhigiter <abhigiter@gmail.com>
2016-07-03 19:12:04 +0530

    added test file

--- stu_a0004/asg_a0043/MainAsgA0043.java
+++ stu_a0004/asg_a0043/MainAsgA0043.java
@@ -3,7 +3,7 @@ public class MainAsgA0043
 public static void main(String args[])
 {
 AsgA0043 temp=new AsgA0043();
-System.out.println(temp.numlen(12345));
+System.out.println(temp.numlen(1234));
 }
 }
 
