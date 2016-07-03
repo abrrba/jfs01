fe47e3f4f4d06fbbf6b5dfe02600c3c4da9cb988
abhigiter <abhigiter@gmail.com>
2016-07-03 19:44:21 +0530

    added testfiles

---
+++ stu_a0004/asg_a0006/TestAsgA0006.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestAsgA0006{
+    @Test
+        public void test_01() {
+            AsgA0006 obj  = new AsgA0006();
+            double temp = obj.get_amount(10000,5,2);
+            assertEquals(temp, 1000);
+        }
+    @Test
+        public void test_02() {
+            AsgA0006 obj = new AsgA0006();
+            double temp = obj.get_amount(1000,0,2);
+            assertEquals(temp, 0);
+        }
+
+    @Test
+        public void test_03() {
+            AsgA0006 obj = new AsgA0006();
+            double temp = obj.get_amount(1000,5,0.5);
+            assertEquals(temp, 25);
+        }
+    
+    @Test
+        public void test_04() {
+            AsgA0006 obj = new AsgA0006();
+            double temp = obj.get_amount(1200.5,5,1);
+            assertEquals(temp, 60.025);
+        }
+}
