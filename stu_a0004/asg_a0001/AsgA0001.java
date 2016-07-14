54aa0b99b95e81aaf8555b884552c557adec5333
abhigiter <abhigiter@gmail.com>
2016-07-14 23:26:27 +0530

    added new folder with test cases

--- stu_a0004/asg_a0001/AsgA0001.java
+++ stu_a0004/asg_a0001/AsgA0001.java
@@ -1,25 +1,28 @@
 class AsgA0001
 {
 	public static String alter_case_1(String str)
-	{
+	{	
+		String t1="";
 		int lim=str.length();
 		for(int i=0;i<lim;i++)
 		{
 			if(i%2==0)
 			{
 				char s=str.charAt(i);
-				//str1=String.valueOf(s);
-				System.out.print(Character.toUpperCase(s));
+				t1=t1+Character.toUpperCase(s);
+				
+				//System.out.print(Character.toUpperCase(s));
 			}
 			else
 			{
 				char t=str.charAt(i);
-				//str1=String.valueOf(t);
-				System.out.print(Character.toLowerCase(t));
+				t1=t1+Character.toLowerCase(t);
+				//return t;
+				//System.out.print(Character.toLowerCase(t));
 				
 			}
 		
 		}
-	return(str);
+	return t1;
 	}
 }
