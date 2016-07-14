1acab5985ed1e7cc7d8c745d99b79ae82daf56a5
AnandGade <gadeanand5@gmail.com>
2016-07-14 21:17:55 +0530

    program

--- stu_a0006/asg_a0031/AsgA0031.java
+++ stu_a0006/asg_a0031/AsgA0031.java
@@ -2,8 +2,15 @@ class AsgA0031
 {
 public static double gross(double bs,double da,double hr)
 {
+if(bs>0)
+{
 double d=(bs*da)/100;
 double salary=(bs+d+hr);
 return(salary);
+}
+else
+{
+return(0);
+}
 }
 }
