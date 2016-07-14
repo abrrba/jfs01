eb1911777d333ec80100fe8f9d273f8e7e711a3c
AnandGade <gadeanand5@gmail.com>
2016-07-14 21:33:38 +0530

    program

---
+++ stu_a0006/asg_a0032/TestAsgA0032.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestAsgA0032{
+public static double delta=1e-15;   
+ @Test
+        public void test_01() {
+            AsgA0032 obj  = new AsgA0032();
+            double temp = obj.km(1000);
+            assertEquals(temp, 1000000,delta);
+        }
+    @Test
+        public void test_02() {
+            AsgA0032 obj = new AsgA0032();
+            double temp = obj.km(-1000);
+            assertEquals(temp, -1000000,delta);
+        }
+
+
+    @Test
+        public void test_03() {
+            AsgA0032 obj = new AsgA0032();
+            double temp = obj.km(0);
+            assertEquals(temp, 0,delta);
+        }
+
+   
+}
