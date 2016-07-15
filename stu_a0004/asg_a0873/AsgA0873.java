19173dfee7bd718c1a40c075ef8b7bf5b97417dc
abhigiter <abhigiter@gmail.com>
2016-07-15 12:16:31 +0530

    added

---
+++ stu_a0004/asg_a0873/AsgA0873.java
@@ -0,0 +1,9 @@
+class AsgA0873
+{
+public static String trim_string(String str)
+{
+//String str=new String(" how are you ");
+String strnew=str.replaceAll("\\s","");
+return strnew;
+}
+}
---
+++ stu_a0004/asg_a0873/MainAsgA0873.java
@@ -0,0 +1,9 @@
+class MainAsgA0873
+{
+public static void main(String args[])
+{
+AsgA0873 temp=new AsgA0873();
+String st=new String("how are you");
+System.out.println(temp.trim_string(st));
+}
+}
