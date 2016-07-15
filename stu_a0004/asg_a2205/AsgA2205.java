c27fc8cf559524d6c2d70a2ce37da788a2824555
abhigiter <abhigiter@gmail.com>
2016-07-15 21:11:11 +0530

    added files

---
+++ stu_a0004/asg_a2205/AsgA2205.java
@@ -0,0 +1,15 @@
+class AsgA2205
+{
+public static int is_vowel(String str)
+{
+//String str=new String("O");
+if(str.equalsIgnoreCase("a")||str.equalsIgnoreCase("e")||str.equalsIgnoreCase("i")||str.equalsIgnoreCase("o")||str.equalsIgnoreCase("u"))
+{
+return(1);
+}
+else
+{
+return(0);
+}
+
+}}
