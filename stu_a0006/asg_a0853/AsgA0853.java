009af88bf3879c0d2659cf893f0b01f96b743017
AnandGade <gadeanand5@gmail.com>
2016-07-17 15:14:36 +0530

    program

--- stu_a0006/asg_a0853/AsgA0853.java
+++ stu_a0006/asg_a0853/AsgA0853.java
@@ -2,19 +2,18 @@ class AsgA0853
 {
 public static int leap(int year)
 {
+
 while(year>1000&&year<=9999)
 {
 int x= year%100;
 int y=x%4;
 if(y==0)
 {
-System.out.println(year+ " is leap year");
-break;
+return (1);
 }
 else
 {
-System.out.println(year+ " is not leap year");
-break;
+return(0);
 }
 }
 return(0);
