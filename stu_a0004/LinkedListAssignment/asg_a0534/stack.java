bbf5748b32f1431e2adb8fa5f6104ef01a445ea9
abhigiter <abhigiter@gmail.com>
2016-08-15 20:55:24 +0530

    Added assignment

---
+++ stu_a0004/LinkedListAssignment/asg_a0534/stack.java
@@ -0,0 +1,86 @@
+class stack
+{
+private int size;
+private ListNode top;
+
+stack()
+{
+top=null;
+size=0;
+}
+
+public boolean isEmpty()
+{
+return top== null;
+}
+
+public int getSize()
+{
+return size;
+}
+
+public void push(int data)
+{
+ListNode temp=new ListNode(data);
+if(top==null)
+{
+top=temp;
+}
+else
+{
+temp.setNext(top);
+top=temp;
+}
+size ++;
+}
+
+    public int pop()
+
+    {
+
+        
+
+        ListNode ptr = top;
+
+        top = ptr.getNext();
+
+        size-- ;
+
+        return ptr.getData();
+
+    }    
+
+
+    public void display()
+
+    {
+
+        System.out.print("\nStack = ");
+
+        if (size == 0) 
+
+        {
+
+            System.out.print("Empty\n");
+
+            return ;
+
+        }
+
+        ListNode ptr = top;
+
+        while (ptr != null)
+
+        {
+
+            System.out.print(ptr.getData()+" ");
+
+            ptr = ptr.getNext();
+
+        }
+
+        System.out.println();        
+
+    }
+
+}
