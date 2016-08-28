228cb562332415ee6b95c7dd67fd021f7a162744
AnandGade <gadeanand5@gmail.com>
2016-08-28 23:42:10 +0530

    test

---
+++ stu_a0006/Test4/MainBST.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Stack;
+class MainBST
+{
+	public static void main(String args[]) throws IOException
+	{
+	try{
+		BST bst=new BST();
+		BST bst1=new BST();
+		BufferedReader br=new BufferedReader(new FileReader("a.txt"));
+		String line=br.readLine();
+		while(line!=null)
+		{
+			String[] str=line.split("|");
+			bst.insert(Integer.parseInt(str[0]),str[1]);
+			bst1.dinsert(Integer.parseInt(str[0]),str[1]);
+			line=br.readLine();
+		}
+		System.out.println(" inorder Order  ");
+		bst.inorder();
+		System.out.println(" Desecnding Order ");
+		bst1.inorder(); 
+	}
+catch(NumberFormatException e)
+{
+System.out.println("roll number format wrong please enter the valid inputs\n");
+System.out.println("the wrong number you entered is\n " + "\n"+ e);
+
+}
+}
+}
